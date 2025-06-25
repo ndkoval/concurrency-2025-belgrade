dcad3dd44b0bddd0dddb4436a64d46ef73d8a76b
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-06-25 12:09:09 +0200

    Make FAABasedQueue thread-safe using CAS loops for enqueue and dequeue operations

--- src/day2/FAABasedQueueSimplified.kt
+++ src/day2/FAABasedQueueSimplified.kt
@@ -10,26 +10,26 @@ class FAABasedQueueSimplified<E> : Queue<E> {
     private val deqIdx = AtomicLong(0)
 
     override fun enqueue(element: E) {
-        // TODO: Increment the counter atomically via Fetch-and-Add.
-        // TODO: Use `getAndIncrement()` function for that.
-        val i = enqIdx.get()
-        enqIdx.set(i + 1)
-        // TODO: Atomically install the element into the cell
-        // TODO: if the cell is not poisoned.
-        infiniteArray.set(i.toInt(), element)
+        while (true) {
+            val i = enqIdx.getAndIncrement()
+            if (infiniteArray.compareAndSet(i.toInt(), null, element)) {
+                return
+            }
+        }
     }
 
     @Suppress("UNCHECKED_CAST")
     override fun dequeue(): E? {
-        // Is this queue empty?
-        if (enqIdx.get() <= deqIdx.get()) return null
-        // TODO: Increment the counter atomically via Fetch-and-Add.
-        // TODO: Use `getAndIncrement()` function for that.
-        val i = deqIdx.get()
-        deqIdx.set(i + 1)
-        // TODO: Try to retrieve an element if the cell contains an
-        // TODO: element, poisoning the cell if it is empty.
-        return infiniteArray.get(i.toInt()) as E
+        while (true) {
+            if (deqIdx.get() >= enqIdx.get()) return null
+            val i = deqIdx.getAndIncrement()
+            if (infiniteArray.compareAndSet(i.toInt(), null, POISONED)) {
+                continue
+            }
+            val result = infiniteArray.get(i.toInt()) as E
+            infiniteArray.set(i.toInt(), null)
+            return result
+        }
     }
 
     override fun validate() {
