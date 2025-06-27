bccf36377bcd0681cd208dea65bd611120ad2a13
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-06-27 11:34:46 +0200

    Implement flat-combining technique for thread-safe operations in `FlatCombiningQueue`

--- src/day4/FlatCombiningQueue.kt
+++ src/day4/FlatCombiningQueue.kt
@@ -17,34 +17,62 @@ open class FlatCombiningQueue<E : Any> : Queue<E> {
         queue.removeFirstOrNull()
     }
 
+    @Suppress("UNCHECKED_CAST")
     private inline fun <R> enqueueOrDequeue(task: Any, operation: () -> R): R {
-        // TODO: Make this code thread-safe using the flat-combining technique.
-        // TODO: 1.  Try to become a combiner by
-        // TODO:     changing `combinerLock` from `false` (unlocked) to `true` (locked).
-        // TODO:     (use `tryAcquireLock()` and `releaseLock()` functions).
-        // TODO: 2a. On success, apply this operation and help others by traversing
-        // TODO:     `tasksForCombiner`, performing the announced operations, and
-        // TODO:      updating the corresponding cells to `Result`.
-        // TODO:      Put the corresponding helping object into `helpOthers()`.
-        // TODO: 2b. If the lock is already acquired, announce this operation in
-        // TODO:     `tasksForCombiner` by replacing a random cell state from
-        // TODO:      `null` with `Dequeue`. Wait until either the cell state
-        // TODO:      updates to `Result` (do not forget to clean it in this case),
-        // TODO:      or `combinerLock` becomes available to acquire.
-        return operation()
+        if (tryAcquireLock()) {
+            val operationResult = operation()
+            helpOthers()
+            releaseLock()
+            return operationResult
+        }
+        var cellIndex: Int
+        while (true) {
+            cellIndex = randomCellIndex()
+            if (tasksForCombiner.compareAndSet(cellIndex, null, task)) {
+                break
+            }
+        }
+        while (true) {
+            if (tasksForCombiner[cellIndex] is Result<*>) {
+                val result = tasksForCombiner[cellIndex] as Result<R>
+                tasksForCombiner.set(cellIndex, null)
+                return result.value
+            }
+            if (tryAcquireLock()) {
+                if (tasksForCombiner[cellIndex] is Result<*>) {
+                    val result = tasksForCombiner[cellIndex] as Result<R>
+                    tasksForCombiner.set(cellIndex, null)
+                    releaseLock()
+                    return result.value
+                }
+                tasksForCombiner.set(cellIndex, null)
+                val operationResult = operation()
+                helpOthers()
+                releaseLock()
+                return operationResult
+            }
+        }
     }
 
+    @Suppress("UNCHECKED_CAST")
     private fun helpOthers() {
-        // TODO: Traverse `tasksForCombiner` and perform the announced operations,
-        // TODO: updating the corresponding cells to `Result`.
+        for (i in 0..<TASKS_FOR_COMBINER_SIZE) {
+            val cur = tasksForCombiner[i]
+            when (cur) {
+                null -> continue
+                is Result<*> -> continue
+                Dequeue -> tasksForCombiner.set(i, Result(queue.removeFirstOrNull()))
+                else -> tasksForCombiner.set(i, Result(queue.add(cur as E)))
+            }
+        }
     }
 
     private fun tryAcquireLock(): Boolean {
-        TODO("Try to acquire combinerLock by changing `combinerLock` from `false` (unlocked) to `true` (locked).")
+        return combinerLock.compareAndSet(false, true)
     }
 
     open fun releaseLock() {
-        TODO("Release combinerLock by changing `combinerLock` to `false` (unlocked).")
+        combinerLock.set(false)
     }
 
     private fun randomCellIndex(): Int =
